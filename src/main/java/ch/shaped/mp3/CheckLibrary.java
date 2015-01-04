1a83c7ce0035418d606af779e42f58e09ba14533
Christof Luethi <christof.luethi@gmail.com>
2015-01-04 03:14:04 +0100

    refactored part 1

--- src/main/java/ch/shaped/mp3/CheckLibrary.java
+++ src/main/java/ch/shaped/mp3/CheckLibrary.java
@@ -1,90 +1,106 @@
 package ch.shaped.mp3;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.mpatric.mp3agic.ID3v2;
-import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.Mp3File;
-import com.mpatric.mp3agic.UnsupportedTagException;
+import ch.shaped.mp3.check.ArtworkCheck;
+import ch.shaped.mp3.check.CheckState;
+import ch.shaped.mp3.check.LibraryCheck;
+import ch.shaped.mp3.library.MP3LibraryAlbum;
+import ch.shaped.mp3.library.MP3LibraryItem;
 
 /**
  * mp3 checker
  */
-public class ID3v2ArtworkChecker {
-	private static final Logger logger = LogManager.getLogger(ID3v2ArtworkChecker.class);
+public class CheckLibrary {
+	private static final Logger logger = LogManager.getLogger(CheckLibrary.class);
 	
-	private Set<String> failFolders = new HashSet<String>();
+	private List<LibraryCheck> checks = new ArrayList<LibraryCheck>();
 	private File source;
 	
-	public ID3v2ArtworkChecker(File source) {
+	public CheckLibrary(File source) {
 		this.source = source;
 	}
 	
 	public void run() {
-		FileCrawler fc = new FileCrawler(source);
-    	List<File> files = fc.scan();
-    	
-    	for (File file : files) {
-    		Mp3File mp3file;
-			try {
-				mp3file = new Mp3File(file.getAbsoluteFile());
-				if(mp3file.hasId3v2Tag()) {
-					ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-        			if(id3v2Tag != null) {
-        				byte[] albumImageData = id3v2Tag.getAlbumImage();
-	        			if(albumImageData == null || albumImageData.length == 0) {
-	        				this.failFolders.add(file.getAbsoluteFile().getParentFile().getName());
-	        				logger.trace(file.getAbsoluteFile() +" has no Album Artwork set");
-	        			}
-        			}
-        		}
-			} catch (UnsupportedTagException e) {
-				logger.error(file.getName() +" UnsupportedTagException");
-			} catch (InvalidDataException e) {
-				// logger.error(file.getName() +" is not in mp3 format");
-			} catch (IOException e) {
-				// logger.error(file.getName() +" IOException");
+		File[] albums = this.source.listFiles();
+		int total = albums.length;
+		int processed = 0;
+		if(albums != null) {
+			logger.info("Your music library contains "+albums.length+" albums");
+			
+			for (File album : albums) {
+				
+				if(processed % 10 == 0) {
+					logger.info("Processed "+processed+"/"+total);
+				}
+				
+				if(album.isFile()) {
+					logger.error("Item "+album.getName()+" is a file. Directory expected.");
+				} else {	
+					File[] tracks = album.listFiles();
+					MP3LibraryAlbum libraryAlbum = new MP3LibraryAlbum(album.getName(), album);
+					for (File track : tracks) {
+						if(track.isDirectory()) {
+							logger.error("Item "+album+"/"+track.getName()+" is a directory. File expected.");
+						} else {
+							MP3LibraryItem albumItem = new MP3LibraryItem(track.getName(), track, libraryAlbum);
+							libraryAlbum.addChild(albumItem);
+						}
+					}
+					
+					for (LibraryCheck check : this.checks) {
+						CheckState s = check.run(libraryAlbum);
+						
+						switch (s) {
+							case FAIL:
+								logger.error(check.getName()+ " failed for album "+libraryAlbum.getName()+"");
+								break;
+							case UNKNOWN:
+								logger.warn(check.getName()+ " could not be run for album "+libraryAlbum.getName()+"");
+								break;
+							case OK:
+								logger.trace(check.getName()+ " passed for album "+libraryAlbum.getName()+"");
+								break;
+							default:
+								break;
+						}
+					}
+				}
+				processed++;
 			}
-		}
-    	
-    	logger.info("Artwork for "+this.failFolders.size() +" folders is missing");
-    	List<String> list = ID3v2ArtworkChecker.asSortedList(failFolders);
-    	
-    	for (String s : list) {
-			logger.warn("No Album Artwork for: "+s);
 		}
 	}
 	
     public static void main( String[] args ) {
     	
-        logger.info(ID3v2ArtworkChecker.class + " started...");
+        logger.info(CheckLibrary.class + " started...");
         
         if(args.length > 0 && args[0].length() > 0) {
         	File f = new File(args[0]);
         	if(f.exists()) {
-        		ID3v2ArtworkChecker checker = new ID3v2ArtworkChecker(f);
+        		CheckLibrary checker = new CheckLibrary(f);
+        		checker.addCheck(new ArtworkCheck());
         		checker.run();
         	} else {
-        		logger.error(ID3v2ArtworkChecker.class + " invalid source_path specified. File or Folder '"+ args[0] +"' does not exist");
+        		logger.error(CheckLibrary.class + " invalid source_path specified. File or Folder '"+ args[0] +"' does not exist");
         	}
         } else {
-        	 	logger.info(ID3v2ArtworkChecker.class + " need param source_path");
+        	 	logger.info(CheckLibrary.class + " need param source_path");
         }
     }
     
+    public void addCheck(LibraryCheck lc) {
+    	this.checks.add(lc);
+    }
     
-    public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
+   /* public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
       List<T> list = new ArrayList<T>(c);
       java.util.Collections.sort(list);
       return list;
-    }
+    }*/
 }
